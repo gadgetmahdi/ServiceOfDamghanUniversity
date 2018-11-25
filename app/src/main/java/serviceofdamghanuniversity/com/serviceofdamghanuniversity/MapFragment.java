0d84361ec4d0db784af8c41589aac6d32fdd6e57
mehdi <mehdivijeh@gmail.com>
2018-11-26 00:28:14 +0330

    add map type support

--- app/src/main/java/serviceofdamghanuniversity/com/serviceofdamghanuniversity/MapFragment.java
+++ app/src/main/java/serviceofdamghanuniversity/com/serviceofdamghanuniversity/MapFragment.java
@@ -1,26 +1,35 @@
 package serviceofdamghanuniversity.com.serviceofdamghanuniversity;
 
 
+import android.animation.Animator;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -43,6 +52,40 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
   @BindView(R.id.floatingActionButton)
   FloatingActionButton floatingActionButton;
 
+  @BindView(R.id.map_type_FAB)
+  FloatingActionButton map_type_FAB;
+
+  @BindView(R.id.map_type_selection)
+  ConstraintLayout map_type_selection;
+
+  @BindView(R.id.map_type_default_background)
+  View map_type_default_background;
+
+  @BindView(R.id.map_type_default)
+  ImageView map_type_default;
+
+  @BindView(R.id.map_type_satellite_background)
+  View map_type_satellite_background;
+
+  @BindView(R.id.map_type_satellite)
+  ImageView map_type_satellite;
+
+  @BindView(R.id.map_type_terrain_background)
+  View map_type_terrain_background;
+
+  @BindView(R.id.map_type_terrain)
+  ImageView map_type_terrain;
+
+  @BindView(R.id.map_type_default_text)
+  TextView map_type_default_text;
+
+  @BindView(R.id.map_type_satellite_text)
+  TextView map_type_satellite_text;
+
+  @BindView(R.id.map_type_terrain_text)
+  TextView map_type_terrain_text;
+
+
   private GoogleMap map;
   private ArrayList<Position> mListPositions = new ArrayList<>();
   private Unbinder unbinder;
@@ -50,13 +93,15 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
   private boolean isSetCameraToMyLocation = false;
   private boolean isMapLoaded = false;
   private int busIdSelected = -1;
-  private Map<Integer, LatLng> hmBusIdAndPos = new HashMap<Integer, LatLng>();
+  private Map<Integer, LatLng> hmBusIdAndPos = new HashMap<>();
 
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+    unbinder = ButterKnife.bind(this, view);
 
 
     if (getActivity() != null) {
@@ -73,6 +118,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         @Override
         public void onMyPositionsProvided(Location location) {
           myLocation = location;
+          floatingActionButton.show();
         }
 
         @Override
@@ -87,15 +133,18 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     }
 
 
-    unbinder = ButterKnife.bind(this, view);
+    MapView mMapView = view.findViewById(R.id.mapView);
+    mMapView.onCreate(savedInstanceState);
 
+    mMapView.onResume(); // needed to get the map to display immediately
 
-    SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
-      .findFragmentById(R.id.map_fragment);
-    if (mapFragment != null) {
-      mapFragment.getMapAsync(this);
+    mMapView.getMapAsync(this);
+
+    try {
+      MapsInitializer.initialize(getActivity().getApplicationContext());
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
 
     return view;
   }
@@ -113,7 +162,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
   }
 
   @OnClick(R.id.floatingActionButton)
-  public void onClick() {
+  public void floatingActionButton() {
     if (myLocation != null) {
       LatLng pos = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
 
@@ -121,25 +170,146 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
       map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
       isSetCameraToMyLocation = true;
-    } else {
-      Toast.makeText(getActivity(), "your location not available.", Toast.LENGTH_SHORT).show();
     }
+  }
+
+  @OnClick(R.id.map_type_FAB)
+  public void map_type_FAB() {
+    // Conduct the animation if the FAB is invisible (window open)
+    if (map_type_FAB.getVisibility() == View.INVISIBLE) {
+
+      // Start animator close and finish at the FAB position
+      Animator animator = null;
+      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+        animator = ViewAnimationUtils.createCircularReveal(map_type_selection,
+          map_type_selection.getWidth() - (map_type_FAB.getWidth() / 2),
+          map_type_FAB.getHeight() / 2,
+          ((float) map_type_selection.getWidth()),
+          map_type_FAB.getWidth() / 2f);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.setDuration(200);
+        animator.start();
+        animator.addListener(new Animator.AnimatorListener() {
+          @Override
+          public void onAnimationStart(Animator animator) {
+
+          }
+
+          @Override
+          public void onAnimationEnd(Animator animator) {
+            map_type_selection.setVisibility(View.INVISIBLE);
+          }
+
+          @Override
+          public void onAnimationCancel(Animator animator) {
+
+          }
+
+          @Override
+          public void onAnimationRepeat(Animator animator) {
+
+          }
+        });
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+          @Override
+          public void run() {
+            map_type_selection.setVisibility(View.VISIBLE);
+          }
+        }, 100);
+        animator.start();
+        map_type_FAB.show();
+
+
+      }
+
+    }
+  }
+
+  @OnClick(R.id.map_type_default)
+  public void map_type_default() {
+    map_type_default_background.setVisibility(View.VISIBLE);
+    map_type_satellite_background.setVisibility(View.INVISIBLE);
+    map_type_terrain_background.setVisibility(View.INVISIBLE);
+    map_type_default_text.setTextColor(Color.BLUE);
+    map_type_satellite_text.setTextColor(Color.parseColor("#808080"));
+    map_type_terrain_text.setTextColor(Color.parseColor("#808080"));
+    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+  }
+
+  @OnClick(R.id.map_type_satellite)
+  public void map_type_satellite() {
+    map_type_default_background.setVisibility(View.INVISIBLE);
+    map_type_satellite_background.setVisibility(View.VISIBLE);
+    map_type_terrain_background.setVisibility(View.INVISIBLE);
+    map_type_default_text.setTextColor(Color.parseColor("#808080"));
+    map_type_satellite_text.setTextColor(Color.BLUE);
+    map_type_terrain_text.setTextColor(Color.parseColor("#808080"));
+    map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+  }
+
+  @OnClick(R.id.map_type_terrain)
+  public void map_type_terrain() {
+    map_type_default_background.setVisibility(View.INVISIBLE);
+    map_type_satellite_background.setVisibility(View.INVISIBLE);
+    map_type_terrain_background.setVisibility(View.VISIBLE);
+    map_type_default_text.setTextColor(Color.parseColor("#808080"));
+    map_type_satellite_text.setTextColor(Color.parseColor("#808080"));
+    map_type_terrain_text.setTextColor(Color.BLUE);
+    map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
   }
 
   @Override
   public void onMapReady(GoogleMap googleMap) {
     isMapLoaded = true;
     map = googleMap;
-    map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    setSettingForMap(map);
 
-    if(getActivity() != null) {
+    if (getActivity() != null) {
       if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
         map.setMyLocationEnabled(true);
         map.getUiSettings().setMyLocationButtonEnabled(false);
       }
     }
-  }
 
+    // When map is initially loaded, determine which map type option to 'select'
+
+    switch (map.getMapType()) {
+      case GoogleMap.MAP_TYPE_SATELLITE:
+        map_type_satellite_background.setVisibility(View.VISIBLE);
+        map_type_satellite_text.setTextColor(Color.BLUE);
+        break;
+
+      case GoogleMap.MAP_TYPE_TERRAIN:
+        map_type_terrain_background.setVisibility(View.VISIBLE);
+        map_type_terrain_text.setTextColor(Color.BLUE);
+        break;
+
+      default:
+        map_type_default_background.setVisibility(View.VISIBLE);
+        map_type_default_text.setTextColor(Color.BLUE);
+        break;
+    }
+
+
+}
+
+  private void setSettingForMap(GoogleMap map) {
+    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    map.getUiSettings().setCompassEnabled(true);
+    map.getUiSettings().setMapToolbarEnabled(true);
+    map.getUiSettings().setZoomGesturesEnabled(true);
+    map.getUiSettings().setScrollGesturesEnabled(true);
+    map.getUiSettings().setTiltGesturesEnabled(true);
+    map.getUiSettings().setRotateGesturesEnabled(true);
+
+    if (getActivity() != null) {
+      map.setMapStyle(
+        MapStyleOptions.loadRawResourceStyle(
+          getActivity(), R.raw.theme_night));
+    }
+  }
 
   private void getPosAndParse() {
     if (mListPositions.size() > 0) {
